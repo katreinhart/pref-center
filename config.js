4a31b92e9ede009e05ca90b356391c7b641a48a6
Kat Reinhart <kat.reinhart@gmail.com>
2017-06-12 18:10:07 -0700

    figure it out finally?

--- config.js
+++
@@ -1,9 +0,0 @@
-const env = process.env;
-
-export const nodeEnv = env.NODE_ENV || 'development';
-
-export const mongoDBcred = 'kat-test:asdf1234';
-
-export default {
-  port: env.PORT || 8080
-};
