47861d12573efacaccb03b2aba0cb5f75b1e2bdb
Kat Reinhart <kat.reinhart@gmail.com>
2017-07-10 17:54:49 -0700

    Debugging heroku...

--- tools/publicServer.js
+++ tools/publicServer.js
@@ -2,7 +2,6 @@ import express from 'express';
 import path from 'path';
 import open from 'open';
 import compression from 'compression';
-import favicon from 'serve-favicon';
 
 /*eslint-disable no-console*/
 
